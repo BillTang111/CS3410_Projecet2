ca46156d3a1185d3981f0b0db921c6f5dd3a572d
BillTang111 <bt294@cornell.edu>
2018-05-12 17:39:28 -0400

    doing last part of func2

--- p6-malloc/malloc/heaplib.c
+++ p6-malloc/malloc/heaplib.c
@@ -70,43 +70,43 @@ typedef struct _block_footer {
  */
 /* -------------------- hl_init ----------------- */
 void hl_init(void *heap, unsigned int heap_size) {
-//check the constraints 
-if(heap_size < MIN_HEAP_SIZE || !heap){
-    printf("Ahh, your input heap_size is wrong \n");
-}
+    //check the constraints 
+    if(heap_size < MIN_HEAP_SIZE || !heap){
+        printf("Ahh, your input heap_size is wrong \n");
+    }
 
-// start setting up the heap
-heap_header* heap_head = (heap_header*) heap;
-heap_head -> heap_size = heap_size;
+    // start setting up the heap
+    heap_header* heap_head = (heap_header*) heap;
+    heap_head -> heap_size = heap_size;
 
-//calc the first block starting point after the heap_header
-void* fst_blk_unAlign = ADD_BYTES(heap_head, sizeof(heap_header));
-unsigned long offset = ALIGN(fst_blk_unAlign) - (unsigned long)fst_blk_unAlign;
-block_header* fst_blk_Align = (block_header *)(ADD_BYTES(heap_head, sizeof(heap_header) + offset));
-heap_head -> fst_block = fst_blk_Align;
+    //calc the first block starting point after the heap_header
+    void* fst_blk_unAlign = ADD_BYTES(heap_head, sizeof(heap_header));
+    unsigned long offset = ALIGN(fst_blk_unAlign) - (unsigned long)fst_blk_unAlign;
+    block_header* fst_blk_Align = (block_header *)(ADD_BYTES(heap_head, sizeof(heap_header) + offset));
+    heap_head -> fst_block = fst_blk_Align;
 
-//initialize the first block
-// block_header
-// avail size = heap_size - sizeof(block_header) - sizeof(block_footer) - |fst_blk_Align|
-size_t size_available = (unsigned long)ADD_BYTES(heap_head, heap_size - sizeof(block_footer)) 
-    - (unsigned long)ADD_BYTES(fst_blk_Align, sizeof(block_header));
-size_t size_avai_aligned = size_available == ALIGN(size_available)?size_available : ALIGN(size_available)-ALIGNMENT;
-fst_blk_Align -> block_size = size_avai_aligned+sizeof(block_footer)+sizeof(block_header);
-fst_blk_Align -> prev = NULL;
-fst_blk_Align -> next = NULL;
+    //initialize the first block
+    // block_header
+    // avail size = heap_size - sizeof(block_header) - sizeof(block_footer) - |fst_blk_Align|
+    size_t size_available = (unsigned long)ADD_BYTES(heap_head, heap_size - sizeof(block_footer)) 
+        - (unsigned long)ADD_BYTES(fst_blk_Align, sizeof(block_header));
+    size_t size_avai_aligned = size_available == ALIGN(size_available)?size_available : ALIGN(size_available)-ALIGNMENT;
+    fst_blk_Align -> block_size = size_avai_aligned+sizeof(block_footer)+sizeof(block_header);
+    fst_blk_Align -> prev = NULL;
+    fst_blk_Align -> next = NULL;
 
-// block_footer
-block_footer* fst_blk_footer = (block_footer*)ADD_BYTES(fst_blk_Align, 
-    fst_blk_Align -> block_size - sizeof(block_footer));
+    // block_footer
+    block_footer* fst_blk_footer = (block_footer*)ADD_BYTES(fst_blk_Align, 
+        fst_blk_Align -> block_size - sizeof(block_footer));
 
-#ifdef PRINT_DEBUG
-	printf("heap starts at addr %p\n", heap);
-	printf("block_header_size is  %d\n", sizeof(block_header));
-    printf("first_block starts at  %p\n", fst_blk_Align);
-    printf("size_avai_aligned is  %d\n", size_avai_aligned);
-    printf("block size is  %d\n", fst_blk_Align -> block_size);
-    printf("fst_blk_footer is  %p\n", fst_blk_footer);
-#endif
+    #ifdef PRINT_DEBUG
+        printf("heap starts at addr %p\n", heap);
+        printf("block_header_size is  %d\n", sizeof(block_header));
+        printf("first_block starts at  %p\n", fst_blk_Align);
+        printf("size_avai_aligned is  %d\n", size_avai_aligned);
+        printf("block size is  %d\n", fst_blk_Align -> block_size);
+        printf("fst_blk_footer is  %p\n", fst_blk_footer);
+    #endif
 
     return;
 }
@@ -150,20 +150,63 @@ block_header* find_block (block_header* lst_start, unsigned int payload_size){
  */
 void *hl_alloc(void *heap, unsigned int block_size) {
 
-//check constraints
-if(block_size == 0){
-    return heap;
-}
+    //check constraints
+    if(block_size == 0){ return heap; }
 
-heap_header* heap_head = (heap_header*) heap;
-block_header* list_head = heap_head -> fst_block;
+    //locate the first block from heap
+    heap_header* heap_head = (heap_header*) heap;
+    block_header* list_head = heap_head -> fst_block;
 
-//find space avallable
-size_t avi_size = list_head -> block_size;
+    //find available block address meeting our "block_size"
+    block_header* tgt_blk = find_block (list_head, block_size);
+    if (tgt_blk == NULL){ return NULL; }
 
+    //find this block's size
+    size_t cur_blk_size = tgt_blk -> block_size;
+    //find the Minimum block size needed for "block_size"
+    size_t Min_size = calc_needed_size(block_size);
 
+    //Modify the memory
+    //If the free block's size is greater than size we needed -> split the block
+    if (cur_blk_size - Min_size > calc_needed_size(0)){
+        //store the old block information for calcution reason
+        size_t old_size = tgt_blk->block_size;
+        block_footer* old_footer = ADD_BYTES(tgt_blk, old_size - sizeof(block_footer));
 
-    return NULL;
+        // update the header of tgt_block
+        tgt_blk -> block_size = Min_size;
+        // indicate the current block as unfreed
+        tgt_blk -> block_size = tgt_blk -> block_size | 1;
+
+        //create a footer for this newly allocated block
+        block_footer* tgt_blk_footer = ADD_BYTES(tgt_blk, Min_size-sizeof(block_footer));
+        tgt_blk_footer -> block_size = Min_size;
+
+        //create a new header for the free splitted block;
+        block_header* splt_header = ADD_BYTES(tgt_blk, Min_size);
+        splt_header -> block_size = old_size - Min_size;
+        splt_header -> prev = tgt_blk -> prev;
+        splt_header -> next = tgt_blk -> next;
+
+        //update the dependencies 
+        if (tgt_blk -> prev != NULL){(tgt_blk -> prev) -> next = splt_header;}
+        if (tgt_blk -> next != NULL){(tgt_blk -> next) -> prev = splt_header;}
+
+        //update the old_footer for now_splitted block
+        old_footer -> block_size = old_size - Min_size;
+
+        //if tgt_block is the starting blk in the heap -> update the heap header 
+        if (tgt_blk -> prev == NULL){
+            heap_head -> fst_block  = splt_header;
+        }
+    }else{ //No need to split; just remove the tgt_blk from the free block list
+
+    }
+
+    //find a pointer to the block of memory for the return
+    void * alloc_block_addr = ADD_BYTES(tgt_blk, ALIGNMENT);
+
+    return alloc_block_addr;
 }
 
 void hl_release(void *heap, void *block) {
